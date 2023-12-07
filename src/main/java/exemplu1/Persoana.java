760f235dbd401eb95d8cf155268017d34b902bd7
Dragu Rebeca Alina <rebe@Dragus-MacBook-Air.local>
2023-12-07 20:08:04 +0200

    Initial commit

---
+++ src/main/java/exemplu1/Persoana.java
@@ -0,0 +1,23 @@
+package exemplu1;
+
+public class Persoana {
+
+    private String nume;
+    private int varsta;
+    public String getNume() {
+        return nume;
+    }
+    public int getVarsta() {
+        return varsta;
+    }
+    public Persoana() {}
+    public Persoana(String nume, int varsta) {
+        super();
+        this.nume = nume;
+        this.varsta = varsta;
+    }
+    @Override
+    public String toString() {
+        return nume + ", " + varsta;
+    }
+}
